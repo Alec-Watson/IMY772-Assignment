0b164947dbc2880d90b1dcbe33011455496ba7d1
Alec Watson <alecwatson15@gmail.com>
2025-04-05 13:08:23 +0200

    calculator tests

--- backend/tests/addition.test.ts
+++
@@ -1,6 +0,0 @@
-test('adds 0A + 05 = 0F', () => {
-    const a = parseInt("0A", 16);
-    const b = parseInt("05", 16);
-    const sum = a + b;
-    expect(sum.toString(16).toUpperCase()).toBe("F");
-  });
